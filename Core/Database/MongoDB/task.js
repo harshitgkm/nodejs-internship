c600b480ee20c47e99d993b62a1b687c1b5cebaa
harshit <harshitpurohit@gkmit.co>
2024-10-03 21:48:54 +0530

    docs: add mongo queries

--- Core/Database/MongoDB/task.js
+++ Core/Database/MongoDB/task.js
@@ -18,3 +18,68 @@ db.movies.aggregate([ {$match: {directors : "William K.L. Dickson" }}, {$project
 4. Write an aggregation query to find the top 3 directors who have directed the most movies in the "Short" genre. Return the directors' names and the number of movies.
 
 db.movies.aggregate([{$match: {genres: "Short" }}, { $unwind: "$directors"  }, {$group: {_id: "$directors", movieCount: {$sum: 1 }} }, {$sort: {movieCount: -1}} , {$limit :3}, {$project:{director: "$_id", movieCount: 1  }  }] )
+
+
+
+6. Write an aggregation query to find the pair of actors who have appeared together in the most number of movies. Return their names and the number of movies they've co-starred in.
+
+db.movies.aggregate([    
+    {
+    $match: {
+      "cast": {
+      $exists: true
+      }
+
+    }
+    },
+
+    {
+      $project: {
+        castPairs: {
+          $reduce: {
+            input: { $range: [0, { $subtract: [{ $size: "$cast" }, 1] }] },
+            initialValue: [],
+            in: {
+              $concatArrays: [
+                "$$value",
+                {
+                  $map: {
+                    input: { $slice: ["$cast", { $add: ["$$this", 1] }, { $size: "$cast" }] },
+                    as: "pairActor",
+                    in: [{ $arrayElemAt: ["$cast", "$$this"] }, "$$pairActor"]
+                  }
+                }
+              ]
+            }
+          }
+        }
+      }
+    },
+    {
+      $unwind: "$castPairs"
+    },
+    {
+      $project: {
+        actorPair: {
+          $let: {
+            vars: {
+              first: { $arrayElemAt: ["$castPairs", 0] },
+              second: { $arrayElemAt: ["$castPairs", 1] }
+            },
+            in: {
+              $cond: { if: { $lt: ["$$first", "$$second"] }, then: ["$$first", "$$second"], else: ["$$second", "$$first"] }
+            }
+          }
+        }
+      }
+    },
+    {
+      $group: {
+        _id: "$actorPair",
+        count: { $sum: 1 }
+      }
+    },
+    {
+      $sort: { count: -1 }
+    }
+  ])
